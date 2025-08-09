0704e95e18e5543aa0df8ac728a0d39dce9cc02a
AldanaPortugalPickling <aldanapickling@gmail.com>
2025-08-09 08:32:40 -0500

    finished

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -21,7 +21,7 @@ document.querySelector('#lastModified').textContent = `Last Modified: ${document
 
 
 const temples = [
-    {
+  {
     templeName: "Aba Nigeria",
     location: "Aba, Nigeria",
     dedicated: "2005, August, 7",
@@ -77,6 +77,33 @@ const temples = [
     imageUrl:
     "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
   },
+  
+  {
+   templeName: "Buenos Aires Argentina",
+   location: "Buenos Aires, Argentina",
+   dedicated: "1986, January, 17",
+   area: 30659,
+   imageUrl:
+   "https://churchofjesuschristtemples.org/assets/img/temples/buenos-aires-argentina-temple/buenos-aires-argentina-temple-12546.jpg"
+ },
+ {
+  templeName: "Santiago Chile",
+  location: "Santiago, Chile",
+  dedicated: "1983, September, 15",
+  area: 20831,
+  imageUrl:
+  "https://churchofjesuschristtemples.org/assets/img/temples/_temp/024-Santiago-Chile-Temple.jpg"
+ },
+ {
+  templeName: "Bogota Colombia",
+  location: "Bogota, Colombia",
+  dedicated: "1999, April, 24",
+  area: 53500,
+  imageUrl:
+  "https://churchofjesuschristtemples.org/assets/img/temples/bogota-colombia-temple/bogota-colombia-temple-7734.jpg"
+  
+ },
+
     
 ];
 
