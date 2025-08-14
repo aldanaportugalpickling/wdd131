c22dc2afdb8bd3ecc56acfe0cb176a48817b2095
AldanaPortugalPickling <aldanapickling@gmail.com>
2025-08-14 16:37:00 -0500

    Finished

---
+++ project/scripts/tours.js
@@ -0,0 +1,8 @@
+
+document.querySelectorAll('.details-btn').forEach(button => {
+    button.addEventListener('click', () => {
+        const details = button.nextElementSibling;
+        details.classList.toggle('hidden');
+        button.textContent = details.classList.contains('hidden') ? 'View Details' : 'Hide Details';
+    });
+});
